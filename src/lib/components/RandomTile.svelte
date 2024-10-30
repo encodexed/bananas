67074631ef861337a89a8531c9b7502f01735e7e
Robbie Gollan <robbie.gollan@leapdev.io>
2024-10-30 16:06:08 +1100

    Consolidate random musings from 30th Oct

---
+++ src/lib/components/RandomTile.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import { changePosition } from '$lib/helpers/dragdrop';
+
+  const ondragstart = (e: DragEvent | null) => {
+    if (!e) return;
+    const ele = e.target;
+    if (!(ele instanceof HTMLElement)) return;
+  };
+
+  const ondragend = (e: MouseEvent | null) => {
+    if (!e) return;
+    const ele = e.target;
+    if (!(ele instanceof HTMLElement)) return;
+    changePosition(ele, e.clientX, e.clientY);
+  };
+
+  const getRandomLetter = () => {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const randomIndex = Math.floor(Math.random() * alphabet.length);
+    return alphabet[randomIndex];
+  };
+</script>
+
+<button
+  type="button"
+  class="move-me"
+  draggable="true"
+  {ondragstart}
+  {ondragend}
+>
+  {getRandomLetter()}
+</button>
+
+<style>
+  .move-me {
+    height: 50px;
+    width: 50px;
+    border: 1px solid black;
+    text-align: center;
+    user-select: none;
+    cursor: grab;
+    border-radius: 10px;
+    background-color: yellow;
+    font-size: 16px;
+    font-weight: bold;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import RandomTile from '$lib/components/RandomTile.svelte';
+</script>
+
+<main>
+  <div class="board"></div>
+  <div class="rack">
+    {#each { length: 20 } as _}
+      <RandomTile />
+    {/each}
+  </div>
+</main>
+
+<style>
+  main {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    gap: 20px;
+    flex-direction: column;
+    align-items: center;
+    background-color: #474747;
+  }
+
+  .board {
+    width: 60%;
+    max-width: 800px;
+    min-width: 300px;
+    height: 60%;
+    max-height: 800px;
+    min-height: 300px;
+    background-color: #dfebec;
+  }
+
+  .rack {
+    background-color: green;
+    border: 2px solid yellow;
+    border-radius: 20px;
+    height: fit-content;
+    padding: 10px;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+    max-width: 600px;
+  }
+</style>
